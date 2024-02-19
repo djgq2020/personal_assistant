ed8e5f1b6d336c5f98a22df578d11d9c11d43b53
unknown <3079762899@qq.com>
2024-02-19 20:15:20 +0800

    Uploaded file

--- start.py
+++ start.py
@@ -1,5 +1,3 @@
 import os
-# os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
-os.system('ls')
-os.system('ls')
+os.system('streamlit run  web_demo.py  --server.address 127.0.0.1 --server.port 6006')
 
