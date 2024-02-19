ba1011ebeed6a6f8fb552c9d3590e97d3946a19c
unknown <3079762899@qq.com>
2024-02-19 19:39:08 +0800

    Uploaded file

--- start.py
+++ start.py
@@ -1,2 +1,5 @@
 import os
-os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
+# os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
+os.system('ls')
+os.system('ls')
+
